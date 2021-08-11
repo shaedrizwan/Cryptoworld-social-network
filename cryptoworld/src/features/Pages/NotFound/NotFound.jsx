48243d0ee864c94ef9df27f90f7d500012996d6c
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-12 00:07:59 +0530

    Feat: Pages added

---
+++ cryptoworld/src/features/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <div>404 - Not found</div>
+        </div>
+    )
+}
+
+export default NotFound
---
+++ cryptoworld/src/features/Pages/Post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            <div>Post page</div>
+        </div>
+    )
+}
+
+export default Post
