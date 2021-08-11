76d276ee3f504b713d697f9fe47c0da4e7b24c4e
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-12 00:08:31 +0530

    Components and page index added

---
+++ cryptoworld/src/features/Pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home/Home'
+import Profile from './Profile/Profile'
+import Login from './Login/Login'
+import Post from './Post/Post'
+import NotFound from './NotFound/NotFound'
+
+export {Home,Profile,Login,Post,NotFound}
