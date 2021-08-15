2642e014755c628a2379163065697e4ee3c1c6c9
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-15 17:01:22 +0530

    Feat: Auth and Post added to store

--- cryptoworld/src/app/store.js
+++ cryptoworld/src/app/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../features/Auth/authSlice';
+import postsReducer from "../features/Pages/Post/postsSlice"
 
 export const store = configureStore({
   reducer: {
+    post:postsReducer,
+    auth:authSlice
   },
 });
