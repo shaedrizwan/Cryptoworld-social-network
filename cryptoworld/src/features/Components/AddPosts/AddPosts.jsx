23141c2003d26c43ab2c174279ada5045d8062c3
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-08-15 17:00:27 +0530

    Feat: Add new post

--- cryptoworld/src/features/Components/AddPosts/AddPosts.jsx
+++ cryptoworld/src/features/Components/AddPosts/AddPosts.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import { useState } from 'react'
+import {useDispatch,useSelector} from 'react-redux'
+import axios from 'axios'
+import { addPost } from '../../Pages/Post/postsSlice'
 import './AddPosts.css'
+import {v4 as uuid} from 'uuid'
 
 function AddPosts() {
     const [data,setData] = useState('')
+    const dispatch = useDispatch()
+    const {user,token} = useSelector(state => state.auth)
+
+    const postButtonPressed = async () =>{
+        try{
+            const response = await axios.post('http://localhost:8000/post/add',{
+                post:data,
+            },{
+                headers:{
+                    authorization: token
+                }
+            })
+            const id = uuid()
+            dispatch(addPost({_id:id,post:data,name:user.firstName,username:user.username,profilePicture:user.profilePicture,likes:[]}))
+            setData("")
+        }catch(err){
+            console.log(err.message)
+        }
+    }
+
     return (
         <div className="add-posts">
             <div className="post-title">What's on your Mind?</div>
             <input className="post-input" type="text" value={data} onChange={(e)=>setData(e.target.value)} placeholder="Share to the community"/>
-            <button className="post-submit" onClick={()=>console.log(data)}>Post</button>
+            <button className="post-submit" onClick={()=>postButtonPressed()}>Post</button>
         </div>
     )
 }
-
+ 
 export default AddPosts
